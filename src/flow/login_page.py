ee565d9eec75b8b68b4903a29f94eb55e96f9e65
Ségolène Penin de La Raudière <segolene.penin-de-la-raudiere@polytechnique.edu>
2024-10-07 10:40:42 +0200

    init equipe

--- src/flow/login_page.py
+++ src/flow/login_page.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     st.header("Choix du nom de l'équipe")
     if st.button("Log in"):
         title = st.text_input("équipe", "astrolabe")
-        st.session_state.equipe = title
+        st.session_state["equipe"] = title

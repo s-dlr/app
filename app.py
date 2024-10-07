3194a067ed4d3bb2dc12aa01e139580c183e1db1
s-dlr <segolene.lr@gmail.com>
2024-10-07 12:06:23 +0200

    navgation

--- app.py
+++ app.py
@@ -10,15 +10,21 @@ from src.variables import *
 from src.flow.arborescence.arborescence import Arborescence
 from src.data.objet import MyObjet
 
+def login():
+    st.header("Choix du nom de l'équipe")
+    team = st.text_input("équipe", "astrolabe")
+    button_login = st.button("Log in")
+    if button_login:
+        st.session_state["equipe"] = team
+        st.rerun(scope="app")
 
 def start_game():
-    st.write("Welcome")
     st.session_state["sql_client"] = ClientSQL(
         connection_name="sql", equipe=st.session_state.equipe
     )
     st.session_state["arborescence"] = Arborescence(arborescence="Programme exemple")
     create_objets(arborescence="Programme exemple")
-    st.switch_page("pages/page_arborescence.py")
+    # st.switch_page("pages/page_arborescence.py")
 
 
 def create_objets(arborescence: str) -> None:
@@ -32,11 +38,8 @@ if __name__ == "__main__":
     # TODO page accueil
     st.set_page_config(initial_sidebar_state="collapsed")
     if "equipe" not in st.session_state:
-        st.header("Choix du nom de l'équipe")
-        team = st.text_input("équipe", "astrolabe")
-        button_login = st.button("Log in")
-        if button_login:
-            st.session_state["equipe"] = team
-            start_game()
+        pg = st.navigation([st.Page(login)])
     else:
         start_game()
+        pg = st.navigation("pages/page_arborescence.py")
+    pg.run()

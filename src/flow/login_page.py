64909cef93e27f6268da6620a6dc46145547774c
Ségolène Penin de La Raudière <segolene.penin-de-la-raudiere@polytechnique.edu>
2024-10-07 10:41:53 +0200

    log in

--- src/flow/login_page.py
+++ src/flow/login_page.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 if __name__ == "__main__":
     st.header("Choix du nom de l'équipe")
-    if st.button("Log in"):
-        title = st.text_input("équipe", "astrolabe")
-        st.session_state["equipe"] = title
+    button_login = st.button("Log in")
+    team = st.text_input("équipe", "astrolabe")
+    if button_login:
+        st.session_state["equipe"] = team

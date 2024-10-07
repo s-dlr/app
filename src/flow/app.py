935e67231a80872a85722398ae06d10b9f26e201
Ségolène Penin de La Raudière <segolene.penin-de-la-raudiere@polytechnique.edu>
2024-10-07 11:14:48 +0200

    path

--- src/flow/app.py
+++ src/flow/app.py
@@ -12,13 +12,18 @@ from src.flow.views.options_view import OptionsView
 from src.flow.views.buy_view import BuyView
 from src.data.objet import MyObjet
 
-def login():
-    st.header("Choix du nom de l'équipe")
-    team = st.text_input("équipe", "astrolabe")
-    button_login = st.button("Log in")
-    if button_login:
-        st.session_state["equipe"] = team
-        st.rerun(scope="app")
+
+def start_game():
+    st.session_state["sql_client"] = ClientSQL(
+        connection_name="sql", equipe=st.session_state.equipe
+    )
+    st.session_state["arborescence"] = Arborescence(arborescence="Programme exemple")
+    create_objets(arborescence="Programme exemple")
+    st.switch_page(
+        "src/flow/page_arborescence.py",
+        title="Arborescence",
+        icon="🧊"
+    )
 
 def create_objets(arborescence: str) -> None:
     df_objets = pd.read_csv(FICHIER_OBJETS[arborescence], sep=";")
@@ -29,29 +34,11 @@ def create_objets(arborescence: str) -> None:
 if __name__ == "__main__":
     # TODO page accueil
     if "equipe" not in st.session_state:
-        pg = st.navigation(
-            [
-                st.Page(
-                    login,
-                    title="Choix équipe",
-                    icon=":material/handyman:"
-                )
-            ]
-        )
+        st.header("Choix du nom de l'équipe")
+        team = st.text_input("équipe", "astrolabe")
+        button_login = st.button("Log in")
+        if button_login:
+            st.session_state["equipe"] = team
+            start_game()
     else:
-        st.session_state["sql_client"] = ClientSQL(
-            connection_name="sql", equipe=st.session_state.equipe
-        )
-        st.session_state["arborescence"] = Arborescence(arborescence="Programme exemple")
-        create_objets(arborescence="Programme exemple")
-        pg = st.navigation(
-            [
-                st.Page(
-                        "page_arborescence.py",
-                        title="Arborescence",
-                        icon="🧊"
-                )
-            ]
-       )
-
-    pg.run()
+        start_game()

bac4285bbdf6e69d30a48816f96a9a74a96df7c7
s-dlr <segolene.lr@gmail.com>
2024-10-09 13:36:46 +0200

    fragment

--- pages/options.py
+++ pages/options.py
@@ -35,30 +35,8 @@ def display_option_data(option):
         st.markdown(f":blue[{EFFET_IMMEDIAT}]")
         display_metrics(effets_immediat_dict, compteurs)
 
-def next_step():
-    # Application des modifications à l'objet
-    selected_option = st.session_state.arborescence.question.get_option_by_text(
-        st.session_state.radio_options
-    )
-    objet_option = st.session_state[selected_option.objet]
-    objet_option.apply_modification(selected_option.modification_objet)
-    # Application des modification au programme
-    # TODO
-    # Prochaine question
-    go_to_next_question()
-    if st.session_state.arborescence.type_question == CHOIX_NOMBRE_UNITE:
-        st.switch_page("pages/buy.py")
-    else:
-        st.rerun()
 
-st.set_page_config(
-    page_title="Options",
-    page_icon="🧊",
-    layout="wide",
-    initial_sidebar_state="collapsed",
-)
-
-with st.form("my_form"):
+def display_question():
     # Contexte et question
     st.title(st.session_state.arborescence.arborescence)
     st.write(st.session_state.arborescence.question.contexte_question)
@@ -82,14 +60,43 @@ with st.form("my_form"):
     # Affichage des données correspondant à chaque option
     columns = st.columns(len(list_options))
     for option, col in zip(list_options, columns):
-        with col.container(border=(st.session_state.radio_options == option.texte_option)):
+        with col.container(
+            border=(st.session_state.radio_options == option.texte_option)
+        ):
             display_option_data(option)
 
-    # Bouton validation
-    st.form_submit_button(
-        type="primary",
-        label="VALIDER",
-        use_container_width=True,
-        on_click=next_step,
-        # disabled=(not st.session_state.radio_options),
+@st.fragment
+def next_step():
+    # Application des modifications à l'objet
+    selected_option = st.session_state.arborescence.question.get_option_by_text(
+        st.session_state.radio_options
     )
+    objet_option = st.session_state[selected_option.objet]
+    objet_option.apply_modification(selected_option.modification_objet)
+    # Application des modification au programme
+    # TODO
+    # Prochaine question
+    go_to_next_question()
+    if st.session_state.arborescence.type_question == CHOIX_NOMBRE_UNITE:
+        st.switch_page("pages/buy.py")
+    else:
+        display_question()
+        st.rerun()
+
+st.set_page_config(
+    page_title="Options",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+# Affichage de la question
+display_question()
+
+# Bouton validation
+st.button(
+    type="primary",
+    label="VALIDER",
+    use_container_width=True,
+    on_click=next_step,
+    disabled=(not st.session_state.radio_options),
+)

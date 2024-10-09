af565aede5ce0675a5ccc8b96af6a98da0b90bbe
s-dlr <segolene.lr@gmail.com>
2024-10-09 12:17:17 +0200

    fix

--- src/flow/navigation.py
+++ src/flow/navigation.py
@@ -18,19 +18,20 @@ def go_to_next_question():
     2. Mise à jour de la vue
     """
     # Etat de l'arborescence
-    current_type = str(st.session_state.arborescence.type_question)
     if st.session_state.arborescence:
         selected_option = st.session_state.arborescence.question.get_option_by_text(
             st.session_state.radio_options
         )
+        current_page = str(st.session_state.arborescence.type_question)
         if selected_option.prochaine_question == 0:
             st.session_state.arborescence.load_data(selected_option.prochaine_question)
         else:
             load_next_arborescence()
     else:
+        current_page = ""
         load_next_arborescence()
     # Affichage
-    if current_type != st.session_state.arborescence.type_question:
+    if current_page != st.session_state.arborescence.type_question:
         if st.session_state.arborescence.type_question == CHOIX_OPTION:
             st.switch_page("pages/options.py")
         elif st.session_state.arborescence.type_question == CHOIX_NOMBRE_UNITE:

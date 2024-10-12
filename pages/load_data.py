ace229fdddfa455d10956733c84432642823dfad
Ségolène Penin de La Raudière <segolene.penin-de-la-raudiere@polytechnique.edu>
2024-10-12 12:07:00 +0200

    dev

--- pages/load_data.py
+++ pages/load_data.py
@@ -8,8 +8,9 @@ st.header("Charger les donneés du prochain programme")
 uploaded_arborescence = st.file_uploader("Choisir une arborescence")
 uploaded_objets = st.file_uploader("Choisir un fichier objet")
 
-with st.button("Charger les données", type="primary"):
+# if st.button("Charger les données", type="primary"):
+if st.page_link("pages/options.py", label="Commencer", icon=":material/settings:"):
     load_next_arborescence(uploaded_arborescence)
     init_objets(uploaded_objets)
 
-st.page_link("pages/options.py", label="Commencer", icon=":material/settings:")
+# st.page_link("pages/options.py", label="Commencer", icon=":material/settings:")

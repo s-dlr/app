b377ea7b9fc15dadd3a545c125decc5289d2dd44
Ségolène Penin de La Raudière <segolene.penin-de-la-raudiere@polytechnique.edu>
2024-10-14 19:26:37 +0200

    prerequis

--- src/data/prerequis.py
+++ src/data/prerequis.py
@@ -3,7 +3,7 @@ Module prérequis
 """
 
 import json
-
+import numpy as np
 
 class Prerequis:
     """
@@ -12,14 +12,11 @@ class Prerequis:
     aux seuils minimaux pour que le prérequi soit vérifié
     """
 
-    europeanisation: float = 0.0
-    niveau_techno: float = 0.0
+    europeanisation: float = -np.inf
+    niveau_techno: float = -np.inf
 
     def __init__(self, prerequis_str: str = ""):
         for prerequis in prerequis_str.split("\n"):
             if len(prerequis.split(":")) == 2:
                 attr, value = prerequis.split(":")
                 setattr(self, attr.strip(), float(value.strip()))
-
-    def to_str(self):
-        return json.dumps(self.__dict__)

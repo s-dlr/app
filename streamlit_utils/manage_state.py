536ed83f188c740f0f2c76f4affc278932f3d0d4
Ségolène Penin de La Raudière <segolene.penin-de-la-raudiere@polytechnique.edu>
2024-10-18 22:22:15 +0200

    fix

--- streamlit_utils/manage_state.py
+++ streamlit_utils/manage_state.py
@@ -58,14 +58,20 @@ def get_objets_from_sql() -> None:
         new_objet = Objet(**row.to_dict())
         st.session_state[row[NOM]] = new_objet
 
+
 def get_indicateurs_from_sql() -> None:
     """
     Mise à jour ou créations des indicateurs à partir de SQL
     """
     df_indicateurs = st.session_state.sql_client.get_last_value("Indicateurs")
-    st.session_state["indicateurs"] = Indicateurs(**df_indicateurs.iloc[0].to_dict())
+    if df_indicateurs.shape[0] > 0:
+        st.session_state["indicateurs"] = Indicateurs(
+            **df_indicateurs.iloc[0].to_dict()
+        )
     df_armee = st.session_state.sql_client.get_last_value("Armee")
-    st.session_state["armee"] = Armee(**df_armee.iloc[0].to_dict())
+    if df_armee.shape[0] > 0:
+        st.session_state["armee"] = Armee(**df_armee.iloc[0].to_dict())
+
 
 def update_indicateurs() -> None:
     """

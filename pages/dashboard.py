6275a7025eb5bb147899b15f8f41e130ddcc02d9
s-dlr <segolene.lr@gmail.com>
2024-10-21 14:55:45 +0200

    fix

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -54,6 +54,25 @@ for equipe, col in zip(display_equipes, st.columns(len(display_equipes))):
             domain={"x": [0, 1], "y": [0, 1]},
         )
     )
+    st.plotly_chart(fig, use_container_width=True)
+
+"""
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ["Group 1", "Group 2", "Group 3"]
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(hist_data, group_labels, bin_size=[0.1, 0.25, 0.5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
+"""
 
 # Lien vers les autres pages
 st.page_link(

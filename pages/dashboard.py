306f52d0db06b8e899ba9196027ecf5706c8cbf6
s-dlr <segolene.lr@gmail.com>
2024-10-21 17:34:30 +0200

    cleaning

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -142,24 +142,6 @@ for equipe, col in zip(display_equipes, st.columns(len(display_equipes))):
         )
         col.image(images_drapeaux, width=30)
 
-"""
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
-
-# Group data together
-hist_data = [x1, x2, x3]
-
-group_labels = ["Group 1", "Group 2", "Group 3"]
-
-# Create distplot with custom bin_size
-fig = ff.create_distplot(hist_data, group_labels, bin_size=[0.1, 0.25, 0.5])
-
-# Plot!
-st.plotly_chart(fig, use_container_width=True)
-"""
-
 # Lien vers les autres pages
 st.page_link(
     "pages/store.py", label="Acheter des unités", icon=":material/shopping_cart:"

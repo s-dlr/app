ccd587cb9dc040ec4845599deb8a83e903a03806
s-dlr <segolene.lr@gmail.com>
2024-10-21 18:38:25 +0200

    layout

--- streamlit_utils/display_functions.py
+++ streamlit_utils/display_functions.py
@@ -138,5 +138,8 @@ def display_gauges_armees(values, modifications: dict = None, shape=None, grid=F
 
         fig.update_layout(
             grid={"rows": 4, "columns": 1, "pattern": "independent"},
+            title_font_size=10,
+            showlegend=True,
+            height=100,
         )
     return fig

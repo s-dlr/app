1a483377e2485793406a25353de9665cefd6e311
s-dlr <segolene.lr@gmail.com>
2024-10-21 18:47:18 +0200

    layout

--- streamlit_utils/display_functions.py
+++ streamlit_utils/display_functions.py
@@ -130,15 +130,13 @@ def display_gauges_armees(values, modifications: dict = None, shape=None, grid=F
             margin=dict(l=50, r=50),
         )
     else:
-        n_plots = 0
         for i, armee in enumerate([TERRE, AIR, MER, RENS]):
             if values.get(armee, 0) > 0:
                 fig.add_trace(get_indicateur(armee, i, 0))
-                n_plots += 1
-
         fig.update_layout(
             grid={"rows": 4, "columns": 1, "pattern": "independent"},
             showlegend=True,
-            height=100 * n_plots,
+            height=300,
+            
         )
     return fig

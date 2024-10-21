5721b465319b7f90b901df16e29cb9edf674d6cc
Ségolène Penin de La Raudière <segolene.penin-de-la-raudiere@polytechnique.edu>
2024-10-21 14:27:57 +0200

    display equipes

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -10,13 +10,13 @@ dashboard_connection = st.connection("astrolabedb", autocommit=True, ttl=1)
 
 # Equipes disponibles
 df_indicateurs = dashboard_connection.query(QUERY_INDICATEURS, ttl=5)
-st.multiselect(
+display_equipes = st.multiselect(
     'Equipes',
     df_indicateurs[EQUIPE].unique()
 )
 
 # Courbes budget
-st.line_chart(df_indicateurs, x=ANNEE, y=BUDGET, color=EQUIPE)
+st.line_chart(df_indicateurs[df_indicateurs[EQUIPE] in display_equipes], x=ANNEE, y=BUDGET, color=EQUIPE)
 
 # Lien vers les autres pages
 st.page_link(

5e46fb2851f745189d19dba917b0ee21ca017e91
s-dlr <segolene.lr@gmail.com>
2024-10-22 21:36:15 +0200

    load_data

--- streamlit_utils/navigation.py
+++ streamlit_utils/navigation.py
@@ -15,9 +15,10 @@ def load_next_arborescence(prochaine_arborescence, num_question=1):
     )
     st.session_state["select_option"] = None
     st.session_state['annee'] =  int(st.session_state.arborescence.question.annee)
-    st.session_state["prochaine_arborescence"] = PROCHAINES_ARBORESCENCE.get(
-        prochaine_arborescence
-    )
+    st.session_state["prochaine_arborescence"] = ""
+    # st.session_state["prochaine_arborescence"] = PROCHAINES_ARBORESCENCE.get(
+    #     prochaine_arborescence
+    # )
     # Création des objets de l'arborescence
     # init_objets(FICHIERS_OBJETS[prochaine_arborescence])
     # init_programmes(FICHIERS_PROGRAMMES[prochaine_arborescence])
